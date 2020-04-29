e3cc653de48bd7546477431be850e9e30de0844c
ad3bay0 <byoextreme@hotmail.com>
2020-04-29 01:27:17 +0100

    chore: init repo

---
+++ src/main/java/com/ad3bay0/rkots/Rkots.java
@@ -0,0 +1,13 @@
+package com.ad3bay0.rkots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rkots {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rkots.class, args);
+	}
+
+}
