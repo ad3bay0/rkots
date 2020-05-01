fffa16340e9c47a98dc930956316da6b6e7a214a
ad3bay0 <byoextreme@hotmail.com>
2020-05-01 05:42:43 +0100

    chore: init jwt tokenauthentication

---
+++ src/main/java/com/ad3bay0/rkots/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.ad3bay0.rkots.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import com.ad3bay0.rkots.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
+    
+}
